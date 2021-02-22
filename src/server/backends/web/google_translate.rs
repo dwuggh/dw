5afd342298d8b90d3b3da8ad1767e33a4b83ac35
dwuggh <dwuggh@gmail.com>
2021-02-22 13:05:06 +0800

    change params in google translate

--- src/server/backends/web/google_translate.rs
+++ src/server/backends/web/google_translate.rs
@@ -18,7 +18,7 @@ pub struct GTrans {
 impl GTrans {
     pub fn new(proxy: Proxy) -> GTrans {
         GTrans {
-            url_free: "https://translate.google.cn/translate_a/single?client=gtx&dt=t&ie=UTF-8&oe=UTF-8&sl=auto".to_owned(),
+            url_free: "https://translate.google.cn/translate_a/single?client=gtx&dt=t".to_owned(),
             url_voice: "https: //translate.google.cn/translate_tts?ie=UTF-8&client=t&prev=input&q={}&tl=en&total=1&idx=0&textlen=4&tk={}".to_owned(),
             proxy
         }
@@ -35,7 +35,13 @@ impl Backend for GTrans {
             client_builder = client_builder.proxy(reqwest::Proxy::https(https_proxy).unwrap());
         }
         let client = client_builder.build().unwrap();
-        let params = [("q", query.text), ("tl", &query.lang_to)];
+        let params = [
+            ("q", &query.text),
+            ("tl", &query.lang_to),
+            ("sl", &"auto".into()),
+            ("ie", &"UTF-8".into()),
+            ("oe", &"UTF-8".into()),
+        ];
 
         let resp = client.post(&self.url_free).form(&params).send().unwrap();
         let resp_data: serde_json::Value = resp.json().unwrap();
